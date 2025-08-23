176f90f79332bbb3c1dc4e492103bb26b1c8e96a
Daan <daan@klrnbk.nl>
2025-08-23 15:34:03 +0200

    chore: project setup

---
+++ plugin/build.gradle.kts
@@ -0,0 +1,50 @@
+plugins {
+    `java-gradle-plugin`
+    kotlin("jvm") version "2.0.0"
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    implementation("com.varabyte.kotter:kotter-jvm:1.2.1")
+    testImplementation("com.varabyte.kotterx:kotter-test-support-jvm:1.2.1")
+}
+
+gradlePlugin {
+    plugins {
+        create("ktchanges") {
+            id = "nl.klrnbk.daan.ktchanges"
+            implementationClass = "nl.klrnbk.daan.ktchanges.KtchangesPlugin"
+        }
+    }
+}
+
+val functionalTestSourceSet =
+    sourceSets.create("functionalTest") {}
+
+configurations["functionalTestImplementation"].extendsFrom(configurations["testImplementation"])
+configurations["functionalTestRuntimeOnly"].extendsFrom(configurations["testRuntimeOnly"])
+
+val functionalTest by tasks.registering(Test::class) {
+    testClassesDirs = functionalTestSourceSet.output.classesDirs
+    classpath = functionalTestSourceSet.runtimeClasspath
+    useJUnitPlatform()
+}
+
+gradlePlugin.testSourceSets.add(functionalTestSourceSet)
+
+tasks.named<Task>("check") {
+    dependsOn(functionalTest)
+}
+
+tasks.named<Test>("test") {
+    useJUnitPlatform()
+}
+
+kotlin { jvmToolchain(22) }
+java { toolchain.languageVersion.set(JavaLanguageVersion.of(22)) }
