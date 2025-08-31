5373a1fd8bc8f70080b6bf57591f95ee22da2981
Daan <daan@klrnbk.nl>
2025-08-31 14:18:22 +0200

    fix: wrong dir

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -3,7 +3,7 @@
 # This script runs all the tests for the project.
 
 echo "Navigating to project directory..."
-cd ../plugin || exit 1
+cd ./plugin || exit 1
 
 echo "Running tests..."
 ./gradlew test
